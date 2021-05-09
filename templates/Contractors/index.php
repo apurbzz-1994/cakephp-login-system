ac20dcc469ad724a4afd5b31dbe7a792b6988021
apurbzz-1994 <akash95august@gmail.com>
2021-05-09 23:47:53 +1000

    style contractors index page

--- templates/Contractors/index.php
+++ templates/Contractors/index.php
@@ -4,36 +4,31 @@
  * @var \App\Model\Entity\Contractor[]|\Cake\Collection\CollectionInterface $contractors
  */
 ?>
-<div class="contractors index content">
-    <?= $this->Html->link(__('New Contractor'), ['controller'=>'Users','action' => 'addContractor'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Contractors') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('user_id') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($contractors as $contractor): ?>
-                <tr>
-                    <td><?= $this->Number->format($contractor->id) ?></td>
-                    <td><?= h($contractor->name) ?></td>
-                    <td><?= $contractor->has('user') ? $this->Html->link($contractor->user->id, ['controller' => 'Users', 'action' => 'view', $contractor->user->id]) : '' ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $contractor->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['controller'=>'Users','action' => 'editContractor', $contractor->user->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['controller'=>'Users','action' => 'delete', $contractor->user->id], ['confirm' => __('Are you sure you want to delete # {0}?', $contractor->name)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
+<div class="row px-5 py-5">
+    <h4>All Contractors</h4>
+</div>
+<div class="row py-5 px-5">
+    <?php foreach ($contractors as $contractor): ?>
+        <div class="col-12 col-md-4 col-lg-4 pt-4">
+        <div class="card">
+            <div class="card-header">
+            <i class="fas fa-user"></i> <?= $contractor->name ?>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Phone Number: <?= $contractor->phone_number ?></p>
+                <p class="card-text">Expertise: <?= $contractor->expertise ?></p>
+                <?= $this->Html->link(__('Edit'), ['controller'=>'Users','action' => 'editContractor', $contractor->user->id], ['class'=>'btn btn-primary']) ?>
+                <?= $this->Form->postLink(__('Delete'), 
+                ['controller'=>'Users','action' => 'delete', $contractor->user->id], 
+                ['confirm' => __('Are you sure you want to delete # {0}?', $contractor->name), 'class'=>'btn btn-danger']) 
+                ?>
+            </div>
+        </div>
+    </div>  
+    <?php endforeach; ?>
+</div>
+<div class="row py-5 px-5">
+<div class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
--- templates/Users/edit_contractor.php
+++ templates/Users/edit_contractor.php
@@ -7,6 +7,11 @@
 <div class="row justify-content-md-center py-5">
     <div class="col-12 col-md-6 col-lg-6 px-5">
             <h2 style = 'padding: 1em 0 1em 0; text-align: center';>Edit Your Details - Contractor</h2>
+            <?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $user->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'btn btn-danger offset-md-11']
+            ) ?>
             <?= $this->Form->create($user) ?>
                 <h4><i class="fas fa-user"></i> Account Information</h4>
                 <div class="pt-4 pl-4">
